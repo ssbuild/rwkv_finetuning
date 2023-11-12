1152b275730a8b2df2a66a71e3d51228ef7c2f96
ssbuild <462304@qq.com>
2023-11-12 10:12:46 +0800

    update
    Signed-off-by: ssbuild <462304@qq.com>

--- data_processer.py
+++ data_processer.py
@@ -65,8 +65,13 @@ class TokenIdsMaker:
     def tunction(cls, tokenizer: PreTrainedTokenizer, config, sup, max_seq_length, examples):
         sptoken = [config.bos_token_id]
         ds = []
-        prefix, examples = examples
-        for sid, (q, a) in enumerate(examples):
+        prefix = None
+        history = []
+        for sid, (role,q,a) in enumerate(examples):
+            if role == 'system':
+                prefix = q
+                continue
+            history += [(q,a)]
             a_ids = tokenizer.encode(text=build_template(q,prefix=prefix,history=examples[:sid]), add_special_tokens=False)
             b_ids = tokenizer.encode(text=a, add_special_tokens=False)
             while len(a_ids) + len(b_ids) > max_seq_length - len(sptoken) - 1:
@@ -97,9 +102,14 @@ class TokenIdsMaker:
         assert sliding_size <= max_seq_length - len(sptoken)
 
         ds = []
-        prefix, examples = examples
-        for sid, (q, a) in enumerate(examples):
-            a_ids = tokenizer.encode(text=build_template(q, prefix=prefix, history=examples[:sid]),add_special_tokens=False)
+        prefix = None
+        history = []
+        for sid, (role,q,a) in enumerate(examples):
+            if role == 'system':
+                prefix = q
+                continue
+            history += [(q,a)]
+            a_ids = tokenizer.encode(text=build_template(q, prefix=prefix, history=history[:-1]),add_special_tokens=False)
             b_ids = tokenizer.encode(text=a, add_special_tokens=False)
             if src_max_length and src_max_length > 0:
                 a_ids = a_ids[:src_max_length]
